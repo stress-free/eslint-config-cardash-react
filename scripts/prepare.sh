23d1954f90ceb1f1e2e9cd5e4526d58c287f325a
Paul Hoang <paulkhoahoang@gmail.com>
2018-06-26 12:24:26 -0700

    Fix prepare script for yarn install (#1)

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
-git config commit.template $PWD/.gitcommit_template
+# Check if the .git repo is present before running git commands
+# We need to make this check because yarn runs the `prepare` script
+# when installing this dependecy, which fails since the .git directory
+# isn't present
+if [ -d ".git" ]; then
+  git config commit.template $PWD/.gitcommit_template
 
-git config alias.sync "!git co master && git pull && git co - && git rebase master"
+  git config alias.sync "!git co master && git pull && git co - && git rebase master"
+fi
+
